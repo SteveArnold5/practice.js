c9cbac9bb8a39bece72a87a2551c4c7616b88a87
Steve Arnold <stevea03@gmail.com>
2013-07-06 11:12:13 -0400

    minor refactor

--- modules/formBuilder/Field.js
+++ modules/formBuilder/Field.js
@@ -2,8 +2,7 @@ function Field(){}
 
 Field.prototype.createElement = function(){
 	var em = new ElementManipulator();
-	var creatorType = this.fieldType + "Creator"
-	var creator = new window[creatorType]();
+	var creator = new window[this.fieldType + "Creator"];
 	var input = creator.create(this);
     var field = em.createElement("<div><span class='fieldLabel'>" + this.label + ":&nbsp;</span><span class='fieldInput'></span></div>");
 	field.find(".fieldInput").append(input);
