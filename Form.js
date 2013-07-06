4b252040e1f648f513b56ace2b44431ee677c60c
Steve Arnold <stevea03@gmail.com>
2013-07-06 09:20:33 -0400

    removing deleted files

--- Form.js
+++
@@ -1,8 +0,0 @@
-function Form(){
-}
-
-Form.prototype.createElement = function(){
-	var em = new ElementManipulator();
-	return em.createElement("<form></form>", this.groups, Group);
-}
-	
--- Group.js
+++
@@ -1,6 +0,0 @@
-function Group(){}
-
-Group.prototype.createElement = function(){
-	var em = new ElementManipulator();
-	return em.createElement("<fieldset></fieldset>", this.fields, Field);
-}
